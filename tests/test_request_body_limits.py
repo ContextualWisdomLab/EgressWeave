7850857f65a3bc1cc91f9d22379f0df278f500d7
Seongho Bae <me@seonghobae.me>
2026-08-04 19:52:57 +0900

    test: normalize request-body import ordering

--- tests/test_request_body_limits.py
+++ tests/test_request_body_limits.py
@@ -13,7 +13,6 @@ from egressweave.request_body_safety import (
     _BoundedSyncRequestStream,
     _enforce_declared_request_size,
 )
-
 from egressweave import EgressNotAllowedError, EgressPolicy
 from egressweave.sync_transport import _PinnedEgressTransport
 from egressweave.transport import _PinnedEgressAsyncTransport
