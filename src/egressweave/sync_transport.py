a3222fbadfa0e6fdfb072cfdd28b83522510ff2a
Seongho Bae <me@seonghobae.me>
2026-08-11 13:05:17 +0900

    security: sanitize synchronous response extensions

--- src/egressweave/sync_transport.py
+++ src/egressweave/sync_transport.py
@@ -37,6 +37,7 @@ from egressweave.response_safety import (
     _enforce_declared_response_size,
     _enforce_response_header_limits,
     _force_identity_accept_encoding,
+    _select_public_response_extensions,
 )
 from egressweave.tls import TLSConfiguration, create_egress_ssl_context
 from egressweave.validation import (
@@ -262,6 +263,9 @@ class _PinnedEgressTransport(httpx.BaseTransport):
                 response.headers,
                 self._policy.max_response_bytes,
             )
+            safe_response_extensions = _select_public_response_extensions(
+                response.extensions
+            )
         except EgressNotAllowedError:
             response_denied = True
         if response_denied:
@@ -277,7 +281,7 @@ class _PinnedEgressTransport(httpx.BaseTransport):
             stream=_BoundedSyncResponseStream(
                 ResponseStream(response.stream), self._policy.max_response_bytes
             ),
-            extensions=response.extensions,
+            extensions=safe_response_extensions,
         )
 
     def close(self) -> None:
